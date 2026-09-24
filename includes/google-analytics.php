de1e8d1b1e75ee125e4d91237aa3919c4b74849a
wasi-2550 <waseem@dogpack.media>
2026-09-24 10:39:29 +0100

    Add shared Google Analytics tag

---
+++ includes/google-analytics.php
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6XNVE1FCD9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-6XNVE1FCD9');
+</script>
